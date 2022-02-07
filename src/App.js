5b4706459c1525b1db6238c922b86ddb40d21b37
Diana P <dian6210@stud.kea.dk>
2022-02-07 21:41:16 +0100

    search filter added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import "./App.css";
 import JSONDATA from "./MOCK_DATA.json";
+import { useState } from "react";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <div className="App">
-      <input type="text" placeholder="Search" />
-      {JSONDATA.map((elem, key) => {
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(event) => {
+          setSearchTerm(event.target.value);
+        }}
+      />
+      {JSONDATA.filter((elem) => {
+        if (searchTerm === "") {
+          return elem;
+        } else if (elem.first_name.toLowerCase().includes(searchTerm.toLowerCase())) {
+          return elem;
+        }
+      }).map((elem, key) => {
         return (
           <div className="user" key={key}>
             {elem.first_name}
