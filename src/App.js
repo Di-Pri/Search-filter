09636e7c0dc438748fb86adff2c2ab2b94627d8b
Diana P <dian6210@stud.kea.dk>
2022-02-07 21:15:10 +0100

    displayed data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import JSONDATA from "./MOCK_DATA.json";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <input type="text" placeholder="Search" />
+      {JSONDATA.map((elem, key) => {
+        return (
+          <div className="user" key={key}>
+            {elem.first_name}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
